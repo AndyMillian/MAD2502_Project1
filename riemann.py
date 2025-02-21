402f9d80cb3d75e401f6a25b6c7ec33d7bd13390
AndyMillian <49721818+AndyMillian@users.noreply.github.com>
2025-02-21 11:04:03 -0500

    Adding simpson

--- riemann.py
+++ riemann.py
@@ -4,17 +4,10 @@ def left_endpoint(x_vals, func):
     dx = np.diff(x_vals)
     return np.sum(func(x_vals[:-1])* dx)
 
-
 def trapezoid(x_vals, func):
     dx = np.diff(x_vals)
     return np.sum((dx/2)* (func(x_vals[:-1])+func(x_vals[1:])))
 
-#def simpson(x_vals, func):
-
-
-x_vals = np.linspace(0, np.pi, 10000)
-func = np.sin
-
-print(left_endpoint(x_vals, func))
-print(trapezoid(x_vals, func))
-#print(simpson(x_vals, func))
+def simpson(x_vals, func):
+    dx = np.diff(x_vals)[0]
+    return (dx/3) * (func(x_vals)[0]+4 * np.sum(func(x_vals)[1:-1:2])+2 * np.sum(func(x_vals)[2:-2:2])+func(x_vals)[-1])
